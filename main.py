55c4acf8dc4d3421168034fb418ba5a4992b9c77
SolodOk <50843282+volchonok1515@users.noreply.github.com>
2022-11-11 15:58:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ticket = int(input("Веедите количество билетов: "))
 age_visitor = 0
 price = 0
-price_discount = 0
 for visitor in range(ticket):
     age_visitor = int(input("Ввведите возраст: "))
     if age_visitor < 18:
@@ -16,4 +15,4 @@ for visitor in range(ticket):
 if ticket >= 3:
     price_discount = price * 0.9
 
-print(price or price_discount)
+print(price)
